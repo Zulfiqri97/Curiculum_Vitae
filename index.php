9271f17c379b61e5978127503fd9ba5933ed9197
Zulfiqri <66582528+Zulfiqri97@users.noreply.github.com>
2021-08-23 00:48:59 +0700

    add

---
+++ index.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="images/z.png" rel="icon">
+  <title>Cv Zulfiqri</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="crossorigin" />
+  <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&amp;family=Roboto:wght@300;400;500;700&amp;display=swap" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&amp;family=Roboto:wght@300;400;500;700&amp;display=swap" media="print" onload="this.media='all'" />
+  <noscript>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&amp;family=Roboto:wght@300;400;500;700&amp;display=swap" />
+  </noscript>
+  <link href="css/font-awesome/css/all.min.css?ver=1.2.1" rel="stylesheet">
+  <link href="css/mdb.min.css?ver=1.2.1" rel="stylesheet">
+  <link href="css/aos.css?ver=1.2.1" rel="stylesheet">
+  <link href="css/main.css?ver=1.2.1" rel="stylesheet">
+  <noscript>
+    <style type="text/css">
+      [data-aos] {
+        opacity: 1 !important;
+        transform: translate(0) scale(1) !important;
+      }
+    </style>
+  </noscript>
+</head>
+
+<body class="bg-light" id="top">
+
+  <header class="d-print-none">
+    <div class="container text-center text-lg-left">
+      <div class="pt-4 clearfix">
+        <h1 class="site-title mb-0">Cv Zulfiqri</h1>
+        <div class="site-nav">
+          <nav role="navigation">
+            <ul class="nav justify-content-center">
+              <li class="nav-item"><a class="nav-link" href="#about" title="About"><span class="menu-title">About</span></a>
+              </li>
+              <li class="nav-item"><a class="nav-link" href="#skills" title="Skills"><span class="menu-title">Skills</span></a>
+              </li>
+
+              <li class="nav-item"><a class="nav-link" href="#education" title="Education"><span class="menu-title">Education</span></a>
+              </li>
+
+
+              <li class="nav-item"><a class="nav-link" href="#contact" title="Contact"><span class="menu-title">Contact</span></a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="page-content">
+    <div class="container">
+      <div class="resume-container">
+        <div class="shadow-1-strong bg-white my-5" id="intro">
+          <div class="bg-info text-white">
+            <div class="cover bg-image"><img src="images/header-background.jpg" />
+              <div class="mask" style="background-color: rgba(0, 0, 0, 0.7);backdrop-filter: blur(2px);">
+                <div class="text-center p-5">
+                  <div class="avatar p-1"><img class="img-thumbnail shadow-2-strong" src="images/fotozufi2.jpg" width="150px" height="150px" /></div>
+                  <div class="header-bio mt-3">
+                    <div data-aos="zoom-in" data-aos-delay="0">
+                      <h2 class="h1">Zulfiqri, S.Kom</h2>
+                      <p>Web Developer</p>
+                    </div>
+                    <div class="header-social mb-3 d-print-none" data-aos="zoom-in" data-aos-delay="200">
+                      <nav role="navigation">
+                        <ul class="nav justify-content-center">
+
+                          </li>
+                          <li class="nav-item"><a class="nav-link" target="blank" href="https://www.facebook.com/zufi.zufi.353" title="Facebook"><i class="fab fa-facebook"></i><span class="menu-title sr-only">Facebook</span></a>
+                          </li>
+                          <li class="nav-item"><a class="nav-link" target="blank" href="https://www.instagram.com/zufieee/" title="Instagram"><i class="fab fa-instagram"></i><span class="menu-title sr-only">Instagram</span></a>
+                          </li>
+                          <li class="nav-item"><a class="nav-link" target="blank" href="https://github.com/Zulfiqri97" title="Github"><i class="fab fa-github"></i><span class="menu-title sr-only">Github</span></a>
+                          </li>
+                        </ul>
+                      </nav>
+                    </div>
+                    <div class="d-print-none"><a class="btn btn-info  shadow-sm mt-1 me-2" target="blank" href="Cv-Zulfiqri-2021.pdf" data-aos="fade-right" data-aos-delay="700"> <i class="fas fa-download fa-lg"> Unduh Cv</i></a><a class="btn btn-success shadow-sm mt-1 me-2" href="intent://send/+6281999239325#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end" data-aos="fade-left" data-aos-delay="700"> <i class="fab fa-whatsapp fa-lg"> Whatsapp</i></a></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="shadow-1-strong bg-white my-5 p-5" id="about">
+          <div class="about-section">
+            <div class="row">
+              <div class="col-md-6">
+                <h2 class="h2 fw-light mb-4">Tentang Saya</h2>
+                <p>Hallo, Saya Zulfiqri</p>
+                <p>spesialisasi dalam pengembang Web Developer, menguasai konsep pembuatan aplikasi berbasis Web. Saya mencari peluang kerja yang memberi kesempatan untuk semakin berkembang.</p>
+
+              </div>
+              <div class="col-md-5 offset-lg-1">
+                <div class="row mt-2">
+                  <h2 class="h2 fw-light mb-4">Bio</h2>
+                  <div class="col-sm-5">
+                    <div class="pb-2 fw-bolder"><i class="far fa-calendar-alt pe-2 text-muted" style="width:24px;opacity:0.85;"></i> Umur</div>
+                  </div>
+                  <div class="col-sm-7">
+                    <div class="pb-2">24</div>
+                  </div>
+                  <div class="col-sm-5">
+                    <div class="pb-2 fw-bolder"><i class="far fa-envelope pe-2 text-muted" style="width:24px;opacity:0.85;"></i> Email</div>
+                  </div>
+                  <div class="col-sm-7">
+                    <div class="pb-2">Zuesi.abfor@gmail.com</div>
+                  </div>
+                  <div class="col-sm-5">
+                    <div class="pb-2 fw-bolder"><i class="fab fa-skype pe-2 text-muted" style="width:24px;opacity:0.85;"></i> Skype</div>
+                  </div>
+                  <div class="col-sm-7">
+                    <div class="pb-2">live:zuesi.abfor</div>
+                  </div>
+                  <div class="col-sm-5">
+                    <div class="pb-2 fw-bolder"><i class="fas fa-phone pe-2 text-muted" style="width:24px;opacity:0.85;"></i> Phone</div>
+                  </div>
+                  <div class="col-sm-7">
+                    <div class="pb-2">+62 81999239325</div>
+                  </div>
+                  <div class="col-sm-5">
+                    <div class="pb-2 fw-bolder"><i class="fas fa-map-marker-alt pe-2 text-muted" style="width:24px;opacity:0.85;"></i> Alamat</div>
+                  </div>
+                  <div class="col-sm-7">
+                    <div class="pb-2">Ujung Harapan Rt001/014 Bahagia, Babelan, Bekasi</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="shadow-1-strong bg-white my-5 p-5" id="skills">
+          <div class="skills-section">
+            <h2 class="h2 fw-light mb-4">Keterampilan Profesional</h2>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="mb-3"><span class="fw-bolder">HTML</span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-info" role="progressbar" data-aos="zoom-in-right" data-aos-delay="100" data-aos-anchor=".skills-section" style="width: 95%;" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">Master</div>
+                  </div>
+                </div>
+                <div class="mb-3"><span class="fw-bolder">CSS</span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-info" role="progressbar" data-aos="zoom-in-right" data-aos-delay="200" data-aos-anchor=".skills-section" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">Expert</div>
+                  </div>
+                </div>
+                <div class="mb-3"><span class="fw-bolder">JavaScript</span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-info" role="progressbar" data-aos="zoom-in-right" data-aos-delay="300" data-aos-anchor=".skills-section" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Advance</div>
+                  </div>
+                </div>
+                <div class="mb-3"><span class="fw-bolder">MySQL</span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-info" role="progressbar" data-aos="zoom-in-right" data-aos-delay="300" data-aos-anchor=".skills-section" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Advance</div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="mb-3"><span class="fw-bolder">Visual Studio Code</span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-secondary" role="progressbar" data-aos="zoom-in-right" data-aos-delay="400" data-aos-anchor=".skills-section" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">Expert</div>
+                  </div>
+                </div>
+                <div class="mb-3"><span class="fw-bolder">Framework Codeigneter </span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-secondary" role="progressbar" data-aos="zoom-in-right" data-aos-delay="400" data-aos-anchor=".skills-section" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Advance</div>
+                  </div>
+                </div>
+                <div class="mb-3"><span class="fw-bolder">Microsoft Office: Word, Excel, PowerPoint </span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-secondary" role="progressbar" data-aos="zoom-in-right" data-aos-delay="500" data-aos-anchor=".skills-section" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Advance</div>
+                  </div>
+                </div>
+                <div class="mb-3"><span class="fw-bolder">Github</span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-secondary" role="progressbar" data-aos="zoom-in-right" data-aos-delay="600" data-aos-anchor=".skills-section" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Advance</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="shadow-1-strong bg-white my-5 p-5" id="experience">
+          <div class="work-experience-section">
+            <h2 class="h2 fw-light mb-4">Pengalaman Kerja</h2>
+
+            <div class="timeline">
+              <div class="timeline-card timeline-card-info" data-aos="fade-in" data-aos-delay="0">
+                <div class="timeline-head px-4 pt-3">
+                  <div class="h5">Home Industri Saluyu | <span class="text-teal text-small"> Sales </span> </div>
+                </div>
+                <div class="timeline-body px-4 pb-4">
+                  <div class="text-muted text-small mb-3">Agust 2015 - Des, 2020</div>
+                  <div>Mengantarkan Produk berupa makanan ringan (Kerupuk Kulit) kepada pelanggan yang sudah di sediakan oleh perusahaan.</div>
+                </div>
+              </div>
+              <div class="timeline-card timeline-card-info" data-aos="fade-in" data-aos-delay="200">
+                <div class="timeline-head px-4 pt-3">
+                  <div class="h5">Paragon Technology And Innovation | <span class="text-teal text-small"> Operator Produksi</span> </div>
+                </div>
+                <div class="timeline-body px-4 pb-4">
+                  <div class="text-muted text-small mb-3">Sept, 2020 - Nov, 2020</div>
+                  <div>Mengemas produk kosmetik dari beberapa merk seperti : Wardah, Emina.</div>
+                </div>
+              </div>
+              <div class="timeline-card timeline-card-info" data-aos="fade-in" data-aos-delay="400">
+                <div class="timeline-head px-4 pt-3">
+                  <div class="h5">Home Industri Saluyu | <span class="text-teal text-small"> Sopir</span> </div>
+                </div>
+                <div class="timeline-body px-4 pb-4">
+                  <div class="text-muted text-small mb-3">Jan, 2021 - Sekarang</div>
+                  <div>Mengantarkan Produk ke area Bekasi sampai Karawang.</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="shadow-1-strong bg-white my-5 p-5" id="education">
+          <div class="education-section">
+            <h2 class="h2 fw-light mb-4">Pendidikan</h2>
+            <div class="timeline">
+              <div class="timeline-card timeline-card-success" data-aos="fade-in" data-aos-delay="0">
+                <div class="timeline-head px-4 pt-3">
+                  <div class="h5">MI At-Taufiq | <span class="text-teal text-small"> Madrasah Ibtidaiyah</span> </div>
+                </div>
+                <div class="timeline-body px-4 pb-4">
+                  <div class="text-muted text-small mb-3">2002 - 2008</div>
+                  <div></div>
+                </div>
+              </div>
+              <div class="timeline-card timeline-card-success" data-aos="fade-in" data-aos-delay="200">
+                <div class="timeline-head px-4 pt-3">
+                  <div class="h5">MTS At-Taufiq | <span class="text-teal text-small"> Madrasah Tsanawiyah</span> </div>
+                </div>
+                <div class="timeline-body px-4 pb-4">
+                  <div class="text-muted text-small mb-3">2008 - 2011</div>
+                  <div></div>
+                </div>
+              </div>
+              <div class="timeline-card timeline-card-success" data-aos="fade-in" data-aos-delay="400">
+                <div class="timeline-head px-4 pt-3">
+                  <div class="h5">MA At-Taqwa 04 | <span class="text-teal text-small"> Madrasah Aliyah</span> </div>
+                </div>
+                <div class="timeline-body px-4 pb-4">
+                  <div class="text-muted text-small mb-3">2011 - 2014</div>
+                </div>
+              </div>
+              <div class="timeline-card timeline-card-success" data-aos="fade-in" data-aos-delay="600">
+                <div class="timeline-head px-4 pt-3">
+                  <div class="h5">Universitas Bhayangkara Jakarta |<span class="text-teal text-small"> S1 Teknik Informatika</span> </div>
+                </div>
+                <div class="timeline-body px-4 pb-4">
+                  <div class="text-muted text-small mb-3">2017 - 2021</div>
+                  <a href="https://ubharajaya.ac.id//" target="blank"> <button type="submit" class="btn btn-outline-primary btn-sm"><i class="fa fa-external-link-alt" aria-hidden="true"> Visit Link</i></button></a>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="shadow-1-strong bg-white my-5 p-5" id="contact">
+          <div class="contant-section">
+            <h2 class="h2 fw-light text mb-4">Kontak</h2>
+            <div class="row mb-4">
+              <div class="col-md-5" data-aos="fade-left" data-aos-delay="200">
+                <div class="mt-1">
+                  <div class="h6"><i class="fas fa-phone pe-2 text-muted" style="width:24px;opacity:0.85;"></i> +62 81999239325</div>
+                  <div class="h6"><i class="far fa-envelope pe-2 text-muted" style="width:24px;opacity:0.85;"></i> Zuesi.abfor@gmail.com</div>
+                </div>
+                <div class="mt-5 d-print-none">
+                  <form action="" method="POST">
+                    <div class="form-outline mb-4">
+                      <input type="email" id="email" name="" class="form-control" required />
+                      <label class="form-label" for="email" aria-required="">Email address</label>
+                    </div>
+                    <div class="form-outline mb-4">
+                      <textarea class="form-control" style="resize: none;" id="message" rows="4" name="message" required></textarea>
+                      <label class="form-label" for="message" name="message">Message</label>
+                    </div>
+                    <label>
+
+                      <button class="btn btn-info btn-block mb-4" type="submit">Send</button>
+                  </form>
+                </div>
+              </div>
+              <div class="col-md-7 d-print-none" data-aos="zoom-in" data-aos-delay="100"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d63464.48692419373!2d106.9834233!3d-6.1935206!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6989079c4a1c0f%3A0x514a0a06a275594d!2sKerupuk%20Kulit%20Saluyu!5e0!3m2!1sen!2sid!4v1629592527489!5m2!1sen!2sid" width="500" height="400" style="border:0;width:100%;" allowfullscreen="" loading="lazy"></iframe></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer class="pt-4 pb-4 text-muted text-center d-print-none">
+    <div class="container">
+      <div class="my-3">
+        <div class="h4">
+          <nav role="navigation">
+            <ul class="nav justify-content-center">
+
+              </li>
+              <li class="nav-item"><a class="nav-link" target="blank" href="https://www.facebook.com/zufi.zufi.353" title="Facebook"><i class="fab fa-facebook"></i><span class="menu-title sr-only">Facebook</span></a>
+              </li>
+              <li class="nav-item"><a class="nav-link" target="blank" href="https://www.instagram.com/zufieee/" title="Instagram"><i class="fab fa-instagram"></i><span class="menu-title sr-only">Instagram</span></a>
+              </li>
+              <li class="nav-item"><a class="nav-link" target="blank" href="https://github.com/Zulfiqri97" title="Github"><i class="fab fa-github"></i><span class="menu-title sr-only">Github</span></a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div class="text-small">
+        <div class="mb-1">&copy; CV Zulfiqri . All rights reserved.</div>
+        <div>2021</div>
+      </div>
+    </div>
+  </footer>
+  <script src="scripts/mdb.min.js?ver=1.2.1"></script>
+  <script src="scripts/aos.js?ver=1.2.1"></script>
+  <script src="scripts/main.js?ver=1.2.1"></script>
+</body>
+
+</html>
